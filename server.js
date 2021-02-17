43ae9772341fd18c60b42169909bb33bd68aaffc
Aman Yadav <56191587+Aman32403@users.noreply.github.com>
2021-02-17 09:20:10 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,34 @@
+//Addition
+
+const add = (x,y) =>{
+    let sum = x+y;
+    console.log(sum);
+}
+add(14,9);
+
+
+//Subtraction
+
+const sub = (x,y) =>{
+    let sub = x-y;
+    console.log(sub);
+}
+sub(14,7);
+
+
+//Multiplication
+
+const mul = (x,y) =>{
+    let mul = x*y;
+    console.log(mul);
+}
+mul(9,6);
+
+
+//Division
+
+const div = (x,y) =>{
+    let div = x/y;
+    console.log(div);
+}
+div(54,6);
